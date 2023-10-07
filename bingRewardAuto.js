0eeeae77ea4584bdc383558dd1313ef8efca8554
yclown <yclown@163.com>
2023-10-07 08:40:02 +0800

    Update bingRewardAuto.js

--- bingRewardAuto.js
+++ bingRewardAuto.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         必应积分自动脚本（bingrewaord_autoscript）
 // @namespace    https://github.com/yclown/myTamperMokey
-// @version      1.0.4
+// @version      1.0.5
 // @description  使用edge搜索，脚本会自动生成搜索字符,循环搜索直到达到指定次数，次数到了之后不再搜索，在搜索页右边可重置或关闭脚本。按F12进入调式模式，切换成手机模式，可执行手机搜索
 // @author       yclown
 // @match        https://cn.bing.com/search?*
 // @match        https://www.bing.com/search?*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bing.com
 // @require      https://code.jquery.com/jquery-3.1.1.min.js
-// @original-script https://greasyfork.org/scripts/468463
+// @original-script https://greasyfork.org/zh-CN/scripts/469680
 // @grant        GM_setValue
 // @grant        GM_getValue
 // @grant        GM_addStyle
